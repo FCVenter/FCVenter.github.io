ebb78d069d44ab6f5bc0a730e2c17e2ad07b4148
FCVenter <fcventer17@gmail.com>
2024-11-03 11:14:56 +0200

    Temp update

---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">HAPPY Birthday Ma!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">Veels geluk met ma ser verjaardag!</h3>
+      <p>Liefste ma,</p>
+      <p>
+        Dankie vir die wonderlike ma wat ma is. As daar 'n trofee was vir die beste ouer in die wereld, het ma almal gewen. 
+      </p>
+      <p className="name">FC</p>
+    </div>
+  </div>
+);
